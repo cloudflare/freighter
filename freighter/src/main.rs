54189259de89a36e10d29b41396f261453d209d2
Noah Kennedy <nkennedy@cloudflare.com>
2023-06-20 12:07:00 -0500

    chore: cleanup and dep minification

--- freighter/src/main.rs
+++ freighter/src/main.rs
@@ -1,6 +1,7 @@
+use anyhow::Context;
 use clap::Parser;
 use freighter_auth::pg_backend::PgAuthClient;
-use freighter_index::postgres_client::PostgreSQLIndex;
+use freighter_index::postgres_client::PgIndexClient;
 use freighter_storage::s3_client::S3StorageClient;
 use metrics_exporter_prometheus::PrometheusBuilder;
 use std::fs::read_to_string;
@@ -9,41 +10,44 @@ mod cli;
 mod config;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
     let args = cli::FreighterArgs::parse();
 
-    let config: config::Config =
-        serde_yaml::from_str(&read_to_string(args.config).unwrap()).unwrap();
+    let config: config::Config = serde_yaml::from_str(
+        &read_to_string(args.config)
+            .context("Failed to read config file from disk, is it present?")?,
+    )
+    .context("Failed to deserialize config file, please make sure its in the right format")?;
+
+    let config::Config { service, db, store } = config;
 
     PrometheusBuilder::new()
         .add_global_label("service", "freighter")
-        .with_http_listener(config.service.metrics_address)
+        .with_http_listener(service.metrics_address)
         .set_buckets(&[
             100e-6, 500e-6, 1e-3, 5e-3, 1e-2, 5e-2, 1e-1, 2e-1, 3e-1, 4e-1, 5e-1, 6e-1, 7e-1, 8e-1,
             9e-1, 1.0, 5.0, 10.0,
         ])
-        .unwrap()
+        .context("Failed to set buckets for prometheus")?
         .install()
-        .unwrap();
+        .context("Failed to install prometheus exporter")?;
 
-    let addr = config.service.address;
+    let addr = service.address;
 
-    let index_client = PostgreSQLIndex::new(config.db.clone()).unwrap();
-    let storage_client = S3StorageClient::new(
-        &config.store.name,
-        config.store.region,
-        config.store.credentials,
-    )
-    .unwrap();
-    let auth_client = PgAuthClient::new(config.db).unwrap();
+    let index_client =
+        PgIndexClient::new(db.clone()).context("Failed to construct index client")?;
+    let storage_client = S3StorageClient::new(&store.name, store.region, store.credentials)
+        .context("Failed to initialize storage client")?;
+    let auth_client = PgAuthClient::new(db).context("Failed to initialize auth client")?;
 
-    let router =
-        freighter_server::router(config.service, index_client, storage_client, auth_client);
+    let router = freighter_server::router(service, index_client, storage_client, auth_client);
+
+    tracing::info!(?addr, "Starting freighter instance");
 
     axum::Server::bind(&addr)
         .serve(router.into_make_service())
         .await
-        .unwrap()
+        .context("Freighter server exited with error")
 }
