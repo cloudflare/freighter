df58990adee99757d364add95d74d554794c092c
Noah Kennedy <nkennedy@cloudflare.com>
2024-07-02 13:07:57 -0500

    auth: allow environment variables for cf access config
    This also tweaks the docker file to default to this.

--- crates/freighter-auth/src/cf_backend.rs
+++ crates/freighter-auth/src/cf_backend.rs
@@ -2,8 +2,8 @@ use crate::cf_access::{CfAccess, UserId};
 use crate::{AuthError, AuthProvider, AuthResult};
 use async_trait::async_trait;
 use freighter_api_types::ownership::response::ListedOwner;
+use http::{header, HeaderMap, StatusCode};
 use serde::Deserialize;
-use http::{header, StatusCode, HeaderMap};
 
 /// Registry auth based on Cloudflare Access, using JsonWebTokens for auth
 pub struct CfAuthProvider {
@@ -29,9 +29,21 @@ impl CfAuthProvider {
 #[derive(Deserialize, Clone)]
 pub struct Config {
     /// `https://<team name>.cloudflareaccess.com`
+    #[serde(default = "default_auth_team_base_url")]
     pub auth_team_base_url: String,
     /// Long hash from overview tab
+    #[serde(default = "default_auth_auth_audience")]
     pub auth_audience: String,
+}
+
+fn default_auth_team_base_url() -> String {
+    std::env::var("FREIGHTER_AUTH_TEAM_BASE_URL")
+        .expect("auth_team_base_url not found in config or environment")
+}
+
+fn default_auth_auth_audience() -> String {
+    std::env::var("FREIGHTER_AUTH_AUDIENCE")
+        .expect("auth_audience not found in config or environment")
 }
 
 #[async_trait]
@@ -63,7 +75,12 @@ impl AuthProvider for CfAuthProvider {
         Ok(())
     }
 
-    async fn remove_owners(&self, token: &str, _users: &[&str], _crate_name: &str, ) -> AuthResult<()> {
+    async fn remove_owners(
+        &self,
+        token: &str,
+        _users: &[&str],
+        _crate_name: &str,
+    ) -> AuthResult<()> {
         self.validated_user_id(token).await?;
         Err(AuthError::Unimplemented)
     }
@@ -89,12 +106,20 @@ impl AuthProvider for CfAuthProvider {
         Ok(())
     }
 
-    async fn auth_index_fetch(&self, token: &str, _all_users_can_read_crates: &str) -> AuthResult<()> {
+    async fn auth_index_fetch(
+        &self,
+        token: &str,
+        _all_users_can_read_crates: &str,
+    ) -> AuthResult<()> {
         self.validated_user_id(token).await?;
         Ok(())
     }
 
-    async fn auth_crate_download(&self, token: &str, _all_users_can_read_crates: &str) -> AuthResult<()> {
+    async fn auth_crate_download(
+        &self,
+        token: &str,
+        _all_users_can_read_crates: &str,
+    ) -> AuthResult<()> {
         self.validated_user_id(token).await?;
         Ok(())
     }
@@ -104,7 +129,10 @@ impl AuthProvider for CfAuthProvider {
         Ok(())
     }
 
-    fn token_from_headers<'h>(&self, headers: &'h HeaderMap) -> Result<Option<&'h str>, StatusCode> {
+    fn token_from_headers<'h>(
+        &self,
+        headers: &'h HeaderMap,
+    ) -> Result<Option<&'h str>, StatusCode> {
         if let res @ Some(_) = crate::default_token_from_headers(headers)? {
             return Ok(res);
         }
@@ -126,7 +154,8 @@ fn cookie_parse() {
     let a = CfAuthProvider::new(Config {
         auth_audience: "...".into(),
         auth_team_base_url: "https://test.example.net".into(),
-    }).unwrap();
+    })
+    .unwrap();
 
     let mut h = http::HeaderMap::new();
     h.insert("cookie", http::HeaderValue::from_static("other.cookie=1; lastViewedForm-TEST={}; JSESSIONID=EE; CF_AppSession=2; CF_Authorization=aaaaaaaaa.bbbbbbb.cccccc; X=1"));
