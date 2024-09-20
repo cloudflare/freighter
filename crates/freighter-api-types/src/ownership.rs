203110c2554b9b426b4e877ffce97ccdd2230263
Leo Blöcher <lblocher@cloudflare.com>
2024-09-20 18:41:26 +0200

    Add missing JSON response bodies to index API
    Without a JSON response, cargo prints out raw serde errors without any
    context to the user even after an operation succeeded.

--- crates/freighter-api-types/src/ownership.rs
+++ crates/freighter-api-types/src/ownership.rs
@@ -1,10 +1,36 @@
 pub mod response {
     #[cfg_attr(feature = "client", derive(serde::Deserialize))]
     #[cfg_attr(feature = "server", derive(serde::Serialize))]
+    pub struct OwnerList {
+        // Array of owners of the crate.
+        pub users: Vec<ListedOwner>,
+    }
+
+    #[cfg_attr(feature = "client", derive(serde::Deserialize))]
+    #[cfg_attr(feature = "server", derive(serde::Serialize))]
     pub struct ListedOwner {
+        // Unique unsigned 32-bit integer of the owner.
         pub id: u32,
+        // The unique username of the owner.
         pub login: String,
+        // Name of the owner.
         #[cfg_attr(any(feature = "client", feature = "server"), serde(default))]
         pub name: Option<String>,
+    }
+
+    #[cfg_attr(feature = "client", derive(serde::Deserialize))]
+    #[cfg_attr(feature = "server", derive(serde::Serialize))]
+    pub struct ChangedOwnership {
+        // Indicates the operation succeeded, always true.
+        pub ok: bool,
+        // A string to be displayed to the user.
+        pub msg: String,
+    }
+
+    impl ChangedOwnership {
+        #[must_use]
+        pub fn with_msg(msg: String) -> Self {
+            Self { ok: true, msg, }
+        }
     }
 }
